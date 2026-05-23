b005d1529286dd86e1f73555d3fd0b2c46ff1da7
AishJoshi-2002 <aishjoshi2002@gmail.com>
2026-05-24 00:23:44 +0530

    Navbar

--- Frontend/obj/Debug/net10.0/Frontend.AssemblyInfo.cs
+++ Frontend/obj/Debug/net10.0/Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e326151e8e4f2490ab11366a5c4c7d4489a91360")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39de94648a0d63d23ac522cb4ae605d7c323105")]
 [assembly: System.Reflection.AssemblyProductAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
